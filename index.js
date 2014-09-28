3a7e5d3fadeb69fca8ab266c76f265315044ee3b
Nick Dima <nick.dima@gmail.com>
2014-09-28 17:32:32 +0200

    use refactored interpolateName function and update tests

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ var loaderUtils = require("loader-utils");
 module.exports = function(content) {
 	this.cacheable && this.cacheable();
 	if(!this.emitFile) throw new Error("emitFile is required from module system");
-	var url = loaderUtils.interpolateName(this, content);
+	var query = loaderUtils.parseQuery(this.query);
+	var options = {
+		context: query.context || this.options.context,
+		content: content
+	};
+	var url = loaderUtils.interpolateName(this, query.name, options);
 	this.emitFile(url, content);
 	return "module.exports = __webpack_public_path__ + " + JSON.stringify(url);
 }
